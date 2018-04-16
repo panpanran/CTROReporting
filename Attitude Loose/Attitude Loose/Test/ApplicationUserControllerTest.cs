d50f451e485deab0aa4fb97bf492bb0b877bceaa
Pan <panr2@nih.gov>
2018-04-16 16:28:47 -0400

    create CTRPReports

--- Attitude Loose/Attitude Loose/Test/ApplicationUserControllerTest.cs
+++ Attitude Loose/Attitude Loose/Test/ApplicationUserControllerTest.cs
@@ -53,78 +53,20 @@ namespace Attitude_Loose.Test
         }
 
         [Test()]
-        public void TurnRoundReport()
+        public void CreateTurnroundReport()
         {
+            CTRPReports reports = new CTRPReports();
             var connString = "Server=localhost;Port=5434;User Id=dwprod;Password=dwprod_at_ctrp17;Database=dw_ctrpn";
 
             using (var conn = new NpgsqlConnection(connString))
             {
                 conn.Open();
 
-                // Retrieve all rows
-                string path = @"C:\Users\panr2\Downloads\DataWarehouse\Turnround Report\Code\test.txt";
-                string querytext = System.IO.File.ReadAllText(path).Replace("startDate","2018-04-01").Replace("endDate","2018-04-13");
-
-                var cmd = new NpgsqlCommand(querytext, conn);
-                NpgsqlDataReader datareader = cmd.ExecuteReader();
-                DataTable inputDT = new DataTable();
-                inputDT.Load(datareader);
-
-                DateTime tsrdate = new DateTime();
-                DateTime receiveddate = new DateTime();
-                DateTime onholddate = new DateTime();
-                DateTime offholddate = new DateTime();
-                DataTable outputDT = inputDT.Clone();
-                outputDT.Columns.Add("overalldurations", typeof(Int32));
-                outputDT.Columns.Add("onholdtime", typeof(Int32));
-                outputDT.Columns.Add("processingtime", typeof(Int32));
-                int overalldurations = 0;
-                int onholdtime = -100;
-                int processingtime = 0;
                 try
                 {
-                    foreach (DataRow row in inputDT.Rows)
-                    {
-                        tsrdate = (DateTime)row["tsrdate"];
-                        receiveddate = (DateTime)row["receiveddate"];
-                        onholddate = string.IsNullOrEmpty(row["onholddate"].ToString()) ? new DateTime(2020, 1, 1) : (DateTime)(row["onholddate"]);
-                        offholddate = string.IsNullOrEmpty(row["offholddate"].ToString()) ? new DateTime(2020, 1, 1) : (DateTime)(row["offholddate"]);
-                        if (tsrdate >= receiveddate)
-                            overalldurations = CTRPFunctions.CountBusinessDays(receiveddate, tsrdate, CTRPFunctions.Holidays);
-                        if (!string.IsNullOrEmpty(row["offholddate"].ToString()) && !string.IsNullOrEmpty(row["offholddate"].ToString()))
-                        {
-                            if (offholddate >= onholddate)
-                            {
-                                onholdtime = CTRPFunctions.CountBusinessDays(onholddate, offholddate, CTRPFunctions.Holidays);
-                            }
-                        }
-                        else
-                        {
-                            onholdtime = 0;
-                        }
-                        processingtime = overalldurations - onholdtime;
-                        if (processingtime > 7)
-                        {
-                            if (!row.Table.Columns.Contains("overalldurations"))
-                            {
-                                row.Table.Columns.Add("overalldurations", typeof(Int32));
-                                row.Table.Columns.Add("onholdtime", typeof(Int32));
-                                row.Table.Columns.Add("processingtime", typeof(Int32));
-                            }
-                            row["overalldurations"] = overalldurations;
-                            row["onholdtime"] = onholdtime;
-                            row["processingtime"] = processingtime;
-
-                            outputDT.ImportRow(row);
-                        }
-                        //CTRPFunctions.SendEmail("dd", "dd", "ran.pan@nih.gov");
-                    }
-
+                    DataTable outputDT = reports.Turnround(conn, "2017-09-01", "2017-10-01");
                     CTRPFunctions.CreateExcelByDataTable(outputDT);
 
-                    //var results = inputDT.AsEnumerable().Where(p => (p.Field<DateTime>("tsrdate") - p.Field<DateTime>("receiveddate")).Days -
-                    //CTRPFunctions.CountBusinessDays(p.Field<DateTime>("receiveddate"), p.Field<DateTime>("tsrdate"), Holidays) > 8
-                    //&& p.Field<DateTime>("receiveddate") > new DateTime(2018, 3, 1));
                 }
                 catch (Exception ex)
                 {
---
+++ Attitude Loose/Attitude Loose/Test/CTRPReports.cs
@@ -0,0 +1,222 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Attitude_Loose.Test
+{
+    public class CTRPReports
+    {
+        private string turnround_original_file = @"C:\Users\panr2\Downloads\DataWarehouse\Turnround Report\Code\amendment_sql.txt";
+
+        public DataTable Turnround(NpgsqlConnection conn, string startDate, string endDate)
+        {
+            return Turnround_Original(conn, startDate, endDate);
+        }
+
+        private DataTable Turnround_Original(NpgsqlConnection conn, string startDate, string endDate)
+        {
+            string querytext = System.IO.File.ReadAllText(turnround_original_file).Replace("startDate", startDate).Replace("endDate", endDate);
+            var cmd = new NpgsqlCommand(querytext, conn);
+            NpgsqlDataReader datareader = cmd.ExecuteReader();
+            DataTable inputDT = new DataTable();
+            inputDT.Load(datareader);
+
+            DateTime tsrdate = new DateTime();
+            DateTime receiveddate = new DateTime();
+            DateTime onholddate = new DateTime();
+            DateTime offholddate = new DateTime();
+            DataTable outputDT = inputDT.Clone();
+            //var results = inputDT.AsEnumerable().Where(p => (p.Field<DateTime>("tsrdate") - p.Field<DateTime>("receiveddate")).Days -
+
+            outputDT.Columns.Add("overalldurations", typeof(Int32));
+            outputDT.Columns.Add("onholdtime", typeof(Int32));
+            outputDT.Columns.Add("processingtime", typeof(Int32));
+            int overalldurations = 0;
+            int onholdtime = -100;
+            int processingtime = 0;
+            try
+            {
+                foreach (DataRow row in inputDT.Rows)
+                {
+                    tsrdate = (DateTime)row["tsrdate"];
+                    receiveddate = (DateTime)row["receiveddate"];
+                    onholddate = string.IsNullOrEmpty(row["onholddate"].ToString()) ? new DateTime(2020, 1, 1) : (DateTime)(row["onholddate"]);
+                    offholddate = string.IsNullOrEmpty(row["offholddate"].ToString()) ? new DateTime(2020, 1, 1) : (DateTime)(row["offholddate"]);
+                    if (tsrdate >= receiveddate)
+                        overalldurations = CTRPFunctions.CountBusinessDays(receiveddate, tsrdate, CTRPFunctions.Holidays);
+                    if (!string.IsNullOrEmpty(row["offholddate"].ToString()) && !string.IsNullOrEmpty(row["offholddate"].ToString()))
+                    {
+                        if (offholddate >= onholddate)
+                        {
+                            onholdtime = CTRPFunctions.CountBusinessDays(onholddate, offholddate, CTRPFunctions.Holidays);
+                        }
+                    }
+                    else
+                    {
+                        onholdtime = 0;
+                    }
+                    processingtime = overalldurations - onholdtime;
+                    //if (processingtime > 7)
+                    //{
+                    if (!row.Table.Columns.Contains("overalldurations"))
+                    {
+                        row.Table.Columns.Add("overalldurations", typeof(Int32));
+                        row.Table.Columns.Add("onholdtime", typeof(Int32));
+                        row.Table.Columns.Add("processingtime", typeof(Int32));
+                    }
+                    row["overalldurations"] = overalldurations;
+                    row["onholdtime"] = onholdtime;
+                    row["processingtime"] = processingtime;
+
+                    outputDT.ImportRow(row);
+
+                    int countDuplicate = outputDT.AsEnumerable().Where(x => x.Field<string>("trialid") == row["trialid"].ToString() &&
+                    x.Field<int>("submission") == Convert.ToInt32(row["trialid"])).Count();
+
+                    //}
+                    //CTRPFunctions.SendEmail("dd", "dd", "ran.pan@nih.gov");
+                }
+
+                var results = inputDT.AsEnumerable().GroupBy(x => new { ID = x.Field<string>("trialid"), Submission = x.Field<int>("submission") }).Select(y => y.First());
+                return results.CopyToDataTable();
+                //var results = inputDT.AsEnumerable().Where(p => (p.Field<DateTime>("tsrdate") - p.Field<DateTime>("receiveddate")).Days -
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        public void Turnround_Amendment(string path, NpgsqlConnection conn, string startDate, string endDate)
+        {
+            string querytext = System.IO.File.ReadAllText(path).Replace("startDate", "2018-04-01").Replace("endDate", "2018-04-13");
+            var cmd = new NpgsqlCommand(querytext, conn);
+            NpgsqlDataReader datareader = cmd.ExecuteReader();
+            DataTable inputDT = new DataTable();
+            inputDT.Load(datareader);
+
+            DateTime tsrdate = new DateTime();
+            DateTime receiveddate = new DateTime();
+            DateTime onholddate = new DateTime();
+            DateTime offholddate = new DateTime();
+            DataTable outputDT = inputDT.Clone();
+            outputDT.Columns.Add("overalldurations", typeof(Int32));
+            outputDT.Columns.Add("onholdtime", typeof(Int32));
+            outputDT.Columns.Add("processingtime", typeof(Int32));
+            int overalldurations = 0;
+            int onholdtime = -100;
+            int processingtime = 0;
+            try
+            {
+                foreach (DataRow row in inputDT.Rows)
+                {
+                    tsrdate = (DateTime)row["tsrdate"];
+                    receiveddate = (DateTime)row["receiveddate"];
+                    onholddate = string.IsNullOrEmpty(row["onholddate"].ToString()) ? new DateTime(2020, 1, 1) : (DateTime)(row["onholddate"]);
+                    offholddate = string.IsNullOrEmpty(row["offholddate"].ToString()) ? new DateTime(2020, 1, 1) : (DateTime)(row["offholddate"]);
+                    if (tsrdate >= receiveddate)
+                        overalldurations = CTRPFunctions.CountBusinessDays(receiveddate, tsrdate, CTRPFunctions.Holidays);
+                    if (!string.IsNullOrEmpty(row["offholddate"].ToString()) && !string.IsNullOrEmpty(row["offholddate"].ToString()))
+                    {
+                        if (offholddate >= onholddate)
+                        {
+                            onholdtime = CTRPFunctions.CountBusinessDays(onholddate, offholddate, CTRPFunctions.Holidays);
+                        }
+                    }
+                    else
+                    {
+                        onholdtime = 0;
+                    }
+                    processingtime = overalldurations - onholdtime;
+                    //if (processingtime > 7)
+                    //{
+                    //    if (!row.Table.Columns.Contains("overalldurations"))
+                    //    {
+                    //        row.Table.Columns.Add("overalldurations", typeof(Int32));
+                    //        row.Table.Columns.Add("onholdtime", typeof(Int32));
+                    //        row.Table.Columns.Add("processingtime", typeof(Int32));
+                    //    }
+                    //    row["overalldurations"] = overalldurations;
+                    //    row["onholdtime"] = onholdtime;
+                    //    row["processingtime"] = processingtime;
+
+                    //    outputDT.ImportRow(row);
+                    //}
+                    //CTRPFunctions.SendEmail("dd", "dd", "ran.pan@nih.gov");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        public void Turnround_Abbreviate(string path, NpgsqlConnection conn, string startDate, string endDate)
+        {
+            string querytext = System.IO.File.ReadAllText(path).Replace("startDate", "2018-04-01").Replace("endDate", "2018-04-13");
+            var cmd = new NpgsqlCommand(querytext, conn);
+            NpgsqlDataReader datareader = cmd.ExecuteReader();
+            DataTable inputDT = new DataTable();
+            inputDT.Load(datareader);
+
+            DateTime tsrdate = new DateTime();
+            DateTime receiveddate = new DateTime();
+            DateTime onholddate = new DateTime();
+            DateTime offholddate = new DateTime();
+            DataTable outputDT = inputDT.Clone();
+            outputDT.Columns.Add("overalldurations", typeof(Int32));
+            outputDT.Columns.Add("onholdtime", typeof(Int32));
+            outputDT.Columns.Add("processingtime", typeof(Int32));
+            int overalldurations = 0;
+            int onholdtime = -100;
+            int processingtime = 0;
+            try
+            {
+                foreach (DataRow row in inputDT.Rows)
+                {
+                    tsrdate = (DateTime)row["tsrdate"];
+                    receiveddate = (DateTime)row["receiveddate"];
+                    onholddate = string.IsNullOrEmpty(row["onholddate"].ToString()) ? new DateTime(2020, 1, 1) : (DateTime)(row["onholddate"]);
+                    offholddate = string.IsNullOrEmpty(row["offholddate"].ToString()) ? new DateTime(2020, 1, 1) : (DateTime)(row["offholddate"]);
+                    if (tsrdate >= receiveddate)
+                        overalldurations = CTRPFunctions.CountBusinessDays(receiveddate, tsrdate, CTRPFunctions.Holidays);
+                    if (!string.IsNullOrEmpty(row["offholddate"].ToString()) && !string.IsNullOrEmpty(row["offholddate"].ToString()))
+                    {
+                        if (offholddate >= onholddate)
+                        {
+                            onholdtime = CTRPFunctions.CountBusinessDays(onholddate, offholddate, CTRPFunctions.Holidays);
+                        }
+                    }
+                    else
+                    {
+                        onholdtime = 0;
+                    }
+                    processingtime = overalldurations - onholdtime;
+                    //if (processingtime > 7)
+                    //{
+                    //    if (!row.Table.Columns.Contains("overalldurations"))
+                    //    {
+                    //        row.Table.Columns.Add("overalldurations", typeof(Int32));
+                    //        row.Table.Columns.Add("onholdtime", typeof(Int32));
+                    //        row.Table.Columns.Add("processingtime", typeof(Int32));
+                    //    }
+                    //    row["overalldurations"] = overalldurations;
+                    //    row["onholdtime"] = onholdtime;
+                    //    row["processingtime"] = processingtime;
+
+                    //    outputDT.ImportRow(row);
+                    //}
+                    //CTRPFunctions.SendEmail("dd", "dd", "ran.pan@nih.gov");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
