b65b5c7d123fc4de1e6886ded37c3e9c44a83a03
Pan <panr2@nih.gov>
2018-04-17 16:30:22 -0400

    Calculate Additional Comments

---
+++ Attitude Loose/Attitude Loose/Test/CTRPConst.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Attitude_Loose.Test
+{
+    public class CTRPConst
+    {
+        public const string connString = "Server=localhost;Port=5434;User Id=dwprod;Password=dwprod_at_ctrp17;Database=dw_ctrpn";
+        public const string turnround_original_file = @"C:\Users\panr2\Downloads\DataWarehouse\Turnround Report\Code\original_sql.txt";
+        public const string turnround_amendment_file = @"C:\Users\panr2\Downloads\DataWarehouse\Turnround Report\Code\amendment_sql.txt";
+        public const string turnround_abbreviated_file = @"C:\Users\panr2\Downloads\DataWarehouse\Turnround Report\Code\abbreviated_sql.txt";
+        public const string turnround_savepath = @"C:\Users\panr2\Downloads\DataWarehouse\Turnround Report\Turnround Report";
+
+    }
+}
