d5b2b25b1f0d925cd9159fe91a0c9248a19520ca
Pan <panr2@nih.gov>
2018-09-11 14:44:27 -0400

    change publish setting and project name

---
+++ CTROReporting/CTROReporting/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute("CTROReportingConfig", typeof(CTRPReporting.Startup))]
+namespace CTRPReporting
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
